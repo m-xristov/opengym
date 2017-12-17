8bcb1e94074c6d602b3d23a83f0b766a36edabce
m-xristov <m.xristov@outlook.com>
2017-12-17 21:00:01 +0200

    Add configuration files for eslint, jscs and fix jscs/eslint errors according to rules set in configuration files.

--- test.js
+++ test.js
@@ -1,8 +1,16 @@
-alert(1);
-// this is a test file
-var a = 1;
-var b = a;
-alert(a);
-if (a === b) {
-    alert("equals");
+var a, b = 1, p,
+    d = 10, f = { obj: 1 },
+    hasBarProperty = f.hasOwnProperty("bar");
+alert( a ); // this is a test file
+if ( a === b ) {
+    alert( "equals" );
+}
+alert( hasBarProperty );
+if ( d === f ) {
+    alert( "asdf" );
+    for ( p in f ) {
+        if (f.hasOwnProperty(p)) {
+            alert(p);
+        }
+    }
 }
