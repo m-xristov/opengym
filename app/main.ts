9a11d554725697fb994a2391853f4a52e4540387
m-xristov <m.xristov@outlook.com>
2018-01-05 18:35:06 +0200

    Change logger

--- app/main.ts
+++ app/main.ts
@@ -2,12 +2,14 @@ import {Person} from "app/person";
 import {DB} from "app/test/db";
 import { Logger } from "app/logger";
 
-var person = new Person(),
+let person = new Person(),
     logger = new Logger(),
     arr = [1, 2, 3];
+
 logger.log('1');
 logger.log('test');
 logger.log({a: 1, b: 2, c: {a: 1}, d: [1,2,3, {k: 123}]});
 logger.log('this is a test');
-logger.log(true);
-//console.log(`${person.name} ${DB.title}`);
+logger.log(person);
+logger.log(logger);
+console.log(`${person.name} ${DB.title}`);
