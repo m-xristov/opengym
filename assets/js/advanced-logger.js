a1a648d34d73fd64c12e6ac0b4e403e881065709
Miroslav Hristov <m.xristov@outlook.com>
2018-10-21 17:40:12 +0300

    Adds documentation comments to log in advanced-logger.js file

--- assets/js/advanced-logger.js
+++ assets/js/advanced-logger.js
@@ -1,5 +1,10 @@
 var advancedLogger;
 
+/**
+ * Logs message
+ * NOTE: For testing purposes only
+ * @param {string} message
+ */
 function log(message) {
     console.log(">>>>>>>>>>>>");
     console.log(message);
